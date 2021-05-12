317bde27f53a10ead9ff01144344f30c1224a929
Eleven <qiuqiujun666@gmail.com>
2021-05-12 10:30:24 -0500

    原版服务器下载支持版本切换

--- server-installer/windows_install.py
+++ server-installer/windows_install.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import abc
+import requests
 
 try:
     import wget
@@ -39,10 +40,25 @@ class Vanilla(ServerInstaller):
     """Vanilla factory.
 
     """
+    def __init__(self):
+        self.version_manifest = {}
+        self.version_json = {}
+        self.download_link = ''
 
-    def install(self):
-        wget.download(url='https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
-                      out='server.jar')
+    def install(self, version='latest'):
+        # 获得 version_manifest，对其遍历查找 version 对应的 version_json，并在 version_json 获取下载链接。
+        self.version_manifest = requests.get('https://launchermeta.mojang.com/mc/game/version_manifest.json').json()
+        if version == 'latest':
+            version = self.version_manifest['latest']['release']
+        for self_item in self.version_manifest['versions']:
+            if not self_item['id'] == version:
+                continue
+            self.version_json = requests.get(self_item['url']).json()
+        if self.version_json == {}:
+            raise Exception('错误的版本号')
+        self.download_link = self.version_json['downloads']['server']['url']
+
+        wget.download(url=self.download_link, out='server.jar')
         os.system('java -Xms1024M -Xmx2048M -jar server.jar nogui')
         replace_file_line('eula.txt', 'eula=false', 'eula=true')
 
@@ -112,28 +128,22 @@ def replace_file_line(file, old_line, new_line):
 
 
 if __name__ == '__main__':
-    # 不会写主菜单，可自行使用接口修改
-    def select_server_type(mcdr=False):
-        if mcdr:
-            MCDR().install()
-            os.chdir('./server/')
+    os.system('python -m pip install --upgrade pip')
+    print('是否选择安装 MCDR [y/N]')
+    select_mcdr = str(input('输入: ') or 'n').lower()
+    if select_mcdr not in {'y', 'yes', 'n', 'no'}:
+        raise Exception('请选择 y 或 n')
+    if select_mcdr in {'y', 'yes'}:
+        MCDR().install()
+        os.chdir('./server/')
 
-        while 1:  # 能用就行（逃
-            choice = str(input('==== 服务器类型 ====\n'
-                               '1. 原版\n'
-                               '2. Fabric\n'
-                               '请输入您的选择 (1/2)') or '1')
-            if choice == '1':
-                Vanilla().install()
-            elif choice == '2':
-                Fabric().install()
-            else:
-                print('输入有误')
-                continue
-            break
-
-    def select_server_attachment_mcdr():
-        choice = str(input('是否选择安装 MCDR (y/N)'))
-        return choice == 'y'
-
-    select_server_type(select_server_attachment_mcdr())
+    print('选择服务器内核 [*Vanilla, Fabric]')
+    select_core = str(input('输入:  ') or 'vanilla').lower()
+    if select_core in {'vanilla', 'v'}:
+        print('请选择服务器版本 [*Latest/自定义版本号]')
+        select_version = str(input('输入: ') or 'latest').lower()
+        Vanilla().install(version=select_version)
+    elif select_core in {'fabric', 'f'}:
+        Fabric().install()
+    else:
+        raise Exception('错误的内核')
